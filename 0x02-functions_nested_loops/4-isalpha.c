73b8b2b6640d972cf027057f45355e45404d124a
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-07-18 08:51:07 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: is the char to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 {

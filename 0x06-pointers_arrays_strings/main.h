6b31414ec682db832673c4c865d8a18b380f1796
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-07-26 10:12:07 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,7 +19,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b,int size);
 
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif

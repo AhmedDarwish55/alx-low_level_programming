b9a98699078e4fb0125258aba55564e77997ddd9
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-08-16 04:43:30 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,6 +8,7 @@
  *
  * Return: ...
  */
+
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
@@ -16,13 +17,15 @@ op_t ops[] = {
 { "*", op_mul },
 { "/", op_div },
 { "%", op_mod },
-{ NULL, NULL }
+{ NULL, NULL },
 };
 int i = 0;
+
 while (i < 5)
 {
 if (strcmp(s, ops[i].op) == 0)
 return (ops[i].f);
+
 i++;
 }
 return (0);

371590bcff09c8564d64c82b6c0ba4c87807b719
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-08-24 11:48:20 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,11 +2,10 @@ section 	.text
 global		main
 		extern		printf
 main:
-		mov 	edi, format
-		xor 	eax, eax
+		mov 	edi, msg
 		call 	printf
 		mov 	eax, 0
-		ret
+
 
 section 	.data
 	msg db 'Hello, Holberton\n', 0xa, 0

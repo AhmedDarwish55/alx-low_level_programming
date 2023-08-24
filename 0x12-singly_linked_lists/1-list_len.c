2f8b495025f48cff99a2e0bb794b671b7612a2ed
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-08-23 23:39:29 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - ...
+ * @h: ...
+ *
+ * Return: number of element of h
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t m = 0;
+while (h)
+{
+m++;
+h = h->next;
+}
+return (m);
+}

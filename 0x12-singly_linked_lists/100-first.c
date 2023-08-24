8da54db909c32852842b389701b7a5472f113f49
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-08-24 12:32:00 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,13 @@
 void _constructor(void) __attribute__ ((constructor));
 
 /**
- * _constructo - ...
+ * _constructo - executes before main()
  *
  * Return: void
  */
 
 void _constructo(void)
 {
-printf("You're beat! and yet, you must allow,\n"
-	"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }

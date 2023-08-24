a868ca99c323adb506d6d3e61f161cf0752d1b05
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-08-24 12:04:56 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
+#include "lists.h"
 #include <stdio.h>
+void _constructor(void) __attribute__ ((constructor));
 
 /**
- * first - ...
- * function is executed
+ * _constructo - ...
+ *
+ * Return: void
  */
 
-void first(void)
+void _constructo(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");

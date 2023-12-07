e5641a281a3e0dc365a3ed6f4cc1443d13bc448f
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-12-07 09:20:24 -0800

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - ...
+ * @h: ...
+ *
+ * Return: the number of modes printed
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t i = 0;
+
+        while (h)
+        {
+                h = h->next;
+                i++;
+        }
+        return (i);
+}

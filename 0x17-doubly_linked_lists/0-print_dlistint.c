452c28214323e85252ee2f8e4f27e9d4d524b41f
AhmedDarwish55 <ahmeddarwish241@gmail.com>
2023-12-07 09:17:23 -0800

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t i = 0;
-while (h)
-{
-if (!h->str)
-printf("%d\n", h->n);
-h = h->next;
-i++;
-}
-return (i);
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
